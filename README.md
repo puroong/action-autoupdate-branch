b5bb01f57748ac50fdffd2f6e84f3fb304960340
fasosnql <nowak_przemek@yahoo.com>
2021-02-04 14:04:32 +0100

    setup action

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # autoupdate-branch
+
+[![Project Status: Active – The project has reached a stable, usable state and is being actively developed.](https://www.repostatus.org/badges/latest/active.svg)](https://www.repostatus.org/#active)
+
+A github action to auto update labeled branches with base branch.
+
+## Usage
+- Add workflow to setup Github Actions:
+```yaml
+jobs:
+  update-branches:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Checkout
+        uses: actions/checkout@v2
+      - name: Update branches
+        uses: brainly/autoupdate-branch
+        id: autoUpdateBranche
+        with:
+          repo-token: "${{ secrets.GITHUB_TOKEN }}"
+          label: 'YOUR LABEL NAME'
+```
+- Add label to your Pull Request
+- That's it, now branches with particular label will be auto updated.
---
+++ src/index.ts
@@ -0,0 +1,60 @@
+import * as core from '@actions/core';
+import {getOctokit, context} from '@actions/github';
+import {wait} from './utils/wait';
+
+async function main() {
+  const token = core.getInput('repo-token');
+  const label = core.getInput('label');
+  const client = getOctokit(token);
+  const baseBranch = context.payload.ref;
+
+  const pullsResponse = await client.pulls.list({
+    ...context.repo,
+    base: baseBranch,
+    state: 'open',
+  });
+
+  const prs = pullsResponse.data;
+
+  for (const pr of prs) {
+    if (pr.labels.find(prLabel => prLabel.name === label)) {
+      let detailedPr = {
+        data: {
+          mergeable: null,
+        },
+      };
+
+      // FIRST - CHECK IF CAN BE MERGED
+      while (detailedPr.data.mergeable === null) {
+        /*
+          Need to wait, because:
+          The value of the mergeable attribute can be true, false, or null. If the value is null,
+          this means that the mergeability hasn't been computed yet, and a background job was started
+          to compute it. Give the job a few moments to complete, and then submit the request again.
+          When the job is complete, the response will include a non-null value for the mergeable attribute.
+
+          Referenced issue: https://stackoverflow.com/questions/30619549/why-does-github-api-return-an-unknown-mergeable-state-in-a-pull-request#:~:text=The%20value%20of%20the%20mergeable,then%20submit%20the%20request%20again.
+        */
+
+        await wait(2000);
+
+        detailedPr = await client.pulls.get({
+          ...context.repo,
+          pull_number: pr.number,
+        });
+      }
+
+      if (detailedPr.data.mergeable === false) {
+        core.setOutput('hasConflicts', true);
+      } else if (detailedPr.data.mergeable === true) {
+        // UPDATE BRANCH
+        await client.pulls.updateBranch({
+          ...context.repo,
+          pull_number: pr.number,
+        });
+      }
+    }
+  }
+}
+
+main().catch(err => `autoupdate-branch action failed: ${err}`);
